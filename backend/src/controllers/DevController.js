fdee74b48a88bc32b2dd5d85023c5eb941082277
ronaldscruz <ronald.scruz16@gmail.com>
2019-08-07 19:12:55 -0300

    rota para adicionar dev pronta; tratamento de erro adicionado

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const Dev = require("../models/Dev");
 
 module.exports = {
     async store(request, response){
@@ -7,9 +8,25 @@ module.exports = {
 
         // Pegando dados do GitHub
         const api_response = await axios.get(`http://api.github.com/users/${username}`)
+        .catch(err => (
+            response.json({msg: "Erro na API do GitHub", error: err})
+        ))
 
-        // Desestruturando a resposta da API do GitHub
+        // Desestruturando a resposta da API do GitHub (o user vem da propria requisição do usuario)
+        const {
+            name,
+            bio,
+            avatar_url: avatar
+        } = api_response.data
 
-        return response.json(api_response.data);
+        console.log(api_response.data)
+
+        // Executando a criação do Dev no banco
+        const storingDev = await Dev.create({ name, user: username, bio, avatar })
+        .catch(err => (
+            response.json({msg: "Erro ao inserir dados no banco.", error: err, data_from_api: api_response.data})
+        ));
+
+        return response.json(storingDev);
     }
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,10 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 
 const server = express();
-mongoose.connect("mongodb+srv://levoid:mawchine17@newsapi-ngu16.mongodb.net/omnistack-week?retryWrites=true&w=majority", {useNewUrlParser: true})
+mongoose.connect("mongodb+srv://levoid:mawchine17@newsapi-ngu16.mongodb.net/omnistack-week?retryWrites=true&w=majority", 
+{
+    useNewUrlParser: true
+})
 
 server.use(express.json())
 server.use(routes);
