49e9455ac513ae95d6ad5e9bb0aec8881c60cbbf
ronaldscruz <ronald.scruz16@gmail.com>
2019-08-07 13:31:08 -0300

    metas estabelecidas; backend iniciado; porta inicial pronta

---
+++ backend/src/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const server = express();
+
+server.get("/", (request, response) => {
+    return response.send("Hello, "+request.query.name);
+});
+
+server.listen(3333);
