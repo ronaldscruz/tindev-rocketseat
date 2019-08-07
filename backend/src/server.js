2cbd91742295912b0332f39065182a08b091d536
ronaldscruz <ronald.scruz16@gmail.com>
2019-08-07 15:31:20 -0300

    conexão ao atlas pronta; rotas movidas para um arquivo separado

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const routes = require("./routes");
 
 const server = express();
+mongoose.connect("mongodb+srv://levoid:mawchine17@newsapi-ngu16.mongodb.net/omnistack-week?retryWrites=true&w=majority", {useNewUrlParser: true})
 
-server.get("/", (request, response) => {
-    return response.send("Hello, "+request.query.name);
-});
+server.use(express.json())
+server.use(routes);
 
 server.listen(3333);
