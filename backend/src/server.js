6117fe31b7319f03e77ae1b82850b34eafda1d3b
ronaldscruz <ronald.scruz16@gmail.com>
2019-08-09 14:51:35 -0300

    cors adicionado. API pronta!

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const routes = require("./routes");
 
@@ -8,8 +9,9 @@ const server = express();
 mongoose.connect("mongodb+srv://levoid:mawchine17@newsapi-ngu16.mongodb.net/omnistack-week?retryWrites=true&w=majority", 
 {
     useNewUrlParser: true
-})
+});
 
+server.use(cors());
 server.use(express.json())
 server.use(routes);
 
