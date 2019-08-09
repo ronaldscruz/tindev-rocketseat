85ecf6711601ce6d13c5d1a979500c705b069468
ronaldscruz <ronald.scruz16@gmail.com>
2019-08-09 14:06:38 -0300

    sistema de likes e matches feito

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -2,12 +2,32 @@ const Dev = require("../models/Dev");
 
 module.exports = {
     async store(request, response){
+        // Retornando parametros da URL(recebe like) e do Header(dá like)
         const { devId } = request.params;
         const { user_id } = request.headers;
 
         const loggedDev = await Dev.findById(user_id);
         const targetDev = await Dev.findById(devId);
 
-        return response.json({log: `${loggedDev.name} liked ${targetDev.name}`});
+        // Retornar erro se o Dev 'likado' não existir
+        if(!targetDev){
+            return response.status(400).json({ error: "Esse dev não existe ;/" });
+        }
+
+        // Se o usuário ja deu like nesse dev, retornar erro
+        if(loggedDev.likes.includes(targetDev._id)){
+            return response.status(400).json({ error: "Você já deu like nesse dev!" });
+        }
+
+        // MATCH!
+        if(targetDev.likes.includes(loggedDev._id)){
+            return response.json({ match: true });
+        }
+
+        // Caso tudo esteja certo, adicionar like ao array de Likes
+        loggedDev.likes.push(devId);
+        await loggedDev.save();
+
+        return response.json(loggedDev);
     }
 };
