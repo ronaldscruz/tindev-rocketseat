7841385980015a673ea3e7bbb26a9d54e7b78778
ronaldscruz <ronald.scruz16@gmail.com>
2019-08-10 13:41:58 -0300

    pagina MAIN recebe o ID do usuario

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-export default function Main(){
+export default function Main({ match }){
     return(
-        <h1>Hello, World!</h1>
+        <div className="main-container">
+            <h1>Hello, World!</h1>
+            <p>Your ID is: {match.params.id}</p>
+        </div>
     );
 }
