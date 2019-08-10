a40e1f63be3b20e54cd0b22e7ffc46b0ba0e33cd
ronaldscruz <ronald.scruz16@gmail.com>
2019-08-10 20:50:15 -0300

    likes e dislikes prontos TODO: funcionalidades avançadas

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
 import api from '../../services/api';
 
 import "./index.css";
@@ -24,28 +25,57 @@ export default function Main({ match }){
 		loadDevs();
 	}, [match.params.id]);
 
+	async function handleLike(id){
+		await api.post(`devs/${id}/likes`, null, {
+			headers: {
+				user_id: match.params.id
+			}
+		});
+
+		setDevs(devs.filter(dev => dev._id !== id));
+	}
+
+	async function handleDislike(id){
+		await api.post(`devs/${id}/dislikes`, null, {
+			headers: {
+				user_id: match.params.id
+			}
+		});
+
+		setDevs(devs.filter(dev => dev._id !== id));
+	}
+
 	return(
 		<div className="main-container">
-			<img src={logo} alt="Tindev"/>
-			<ul>
-				{devs.map(dev => (
-					<li>
-						<img src={dev.avatar || "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fmoonvillageassociation.org%2Fwp-content%2Fuploads%2F2018%2F06%2Fdefault-profile-picture1-744x744.jpg&f=1"} alt={`Foto de ${dev.name || "desconhecido"}`}/>
-						<footer>
-							<strong>{dev.name || "Tindev vazio :("}</strong>
-							<p>{dev.bio || "Que tal convidar novos amigos para cá?"}</p>
-						</footer>
-						<div className="rating-buttons">
-							<button type="button">
-								<img src={like} alt="Like"/>
-							</button>
-							<button type="button">
-								<img src={dislike} alt="Dislike"/>
-							</button>
-						</div>
-					</li>
-				))}	
-			</ul>
+			<Link to="/"><img src={logo} alt="Tindev"/></Link>
+				{devs.length > 0 ? 
+					(
+						<ul>
+							{devs.map(dev => (
+								<li key={dev._id}>
+									<img src={dev.avatar || "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fmoonvillageassociation.org%2Fwp-content%2Fuploads%2F2018%2F06%2Fdefault-profile-picture1-744x744.jpg&f=1"} alt={`Foto de ${dev.name || "desconhecido"}`}/>
+									<footer>
+										<strong>{dev.name || "Tindev vazio :("}</strong>
+										<p>{dev.bio || <i>Descrição vazia.</i>}</p>
+									</footer>
+									<div className="rating-buttons">
+										<button type="button" onClick={() => handleLike(dev._id)} >
+											<img src={like} alt="Like"/>
+										</button>
+										<button type="button" onClick={() => handleDislike(dev._id)}>
+											<img src={dislike} alt="Dislike"/>
+										</button>
+									</div>
+								</li>
+							))}
+						</ul>
+					)
+					:
+					(
+						<div className="empty-tindev">Seu Tindev está vazio :(</div>
+					)
+				}
+			
 		</div>
 	);
 }
