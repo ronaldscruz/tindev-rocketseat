c1cd24a702bd68ccdbecdab9072c746cd0ad490c
ronaldscruz <ronald.scruz16@gmail.com>
2019-08-10 13:41:01 -0300

    login verificando usuario na API

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
+import api from '../../services/api';
 
 import './index.css';
 import logo from '../../assets/logo.svg';
 
-export default function Login(){
+export default function Login({ history }){
+    const [username, setUsername] = useState('');
+
+    async function handleSubmit(e){
+        e.preventDefault();
+
+        const api_response = await api.post("devs", {
+            username
+        });
+        
+        const { _id } = api_response.data;
+
+        history.push(`/dev/${_id}`);
+    }
+
     return(
         <div className="login-container">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <img src={logo} alt="Tindev"/>
-                <input type="text" placeholder="Digite seu usuário no GitHub"/>
+                <input
+                    type="text"
+                    placeholder="Digite seu usuário no GitHub"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
                 <button type="submit">Enviar</button>
             </form>
         </div>
