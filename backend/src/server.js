a434197f55292de28cb166159f4fd43aa35f70c6
ronaldscruz <ronald.scruz16@gmail.com>
2019-08-14 13:59:02 -0300

    socket.io passando as conexões para o front

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,15 +4,33 @@ const cors = require("cors");
 
 const routes = require("./routes");
 
-const server = express();
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
 
 mongoose.connect("mongodb+srv://levoid:mawchine17@newsapi-ngu16.mongodb.net/omnistack-week?retryWrites=true&w=majority", 
 {
     useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json())
-server.use(routes);
+app.use((request, response, next) => {
+    request.io = io;
+    request.connectedUsers = connectedUsers;
+
+    return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
